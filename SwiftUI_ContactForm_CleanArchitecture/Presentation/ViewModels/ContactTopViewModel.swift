40f8d26a00211f7056818ed7a83ed0aa28456173
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-12-19 00:03:31 +0900

    お問い合わせTop画面作成開始

---
+++ SwiftUI_ContactForm_CleanArchitecture/Presentation/ViewModels/ContactTopViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContactTopViewModel.swift
+//  SwiftUI_ContactForm_CleanArchitecture
+//
+//  Created by Hiroaki-Hirabayashi on 2021/12/18.
+//
+
+import Foundation
+import Combine
+
+class ContactTopViewModel: ObservableObject {
+
+}
---
+++ SwiftUI_ContactForm_CleanArchitecture/Presentation/Views/Contact/ContactTopView.swift
@@ -0,0 +1,21 @@
+//
+//  ContactTopView.swift
+//  SwiftUI_ContactForm_CleanArchitecture
+//
+//  Created by Hiroaki-Hirabayashi on 2021/12/18.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContactTopView: View {
+    var body: some View {
+        Text("ContactTopView")
+    }
+}
+
+struct ContactTopView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactTopView()
+    }
+}
