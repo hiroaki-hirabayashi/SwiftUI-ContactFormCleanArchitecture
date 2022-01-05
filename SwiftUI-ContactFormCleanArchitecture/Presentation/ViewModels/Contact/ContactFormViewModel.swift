4dd4003d9627393f5f08ef25ae3b1e985eb92f4f
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-01-06 00:43:11 +0900

    TextEditorに文字数制限、Combineのサンプルファイル追加

--- SwiftUI-ContactFormCleanArchitecture/Presentation/ViewModels/Contact/ContactFormViewModel.swift
+++ SwiftUI-ContactFormCleanArchitecture/Presentation/ViewModels/Contact/ContactFormViewModel.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Hiroaki-Hirabayashi on 2021/12/22.
 //
-
+import Combine
 import Foundation
 
 final class ContactFormViewModel: ObservableObject {
     @Published var text = ""
+    @Published var toValidation = false
+    private var anyCancellable = Set<AnyCancellable>()
     private let inquiryUseCase: InquiryUseCaseProtocol = DIContainer.shared.getInquiryUseCase()
     
     func sendInquiry(_ completed: @escaping(_ error: Error?) -> Void) {
@@ -16,5 +18,19 @@ final class ContactFormViewModel: ObservableObject {
         inquiryUseCase.sendInquiry(inquiry: mapper.viewModelToDomain(model: text)) { error in
             completed(error)
         }
+    }
+    
+    // TextEditor 0文字と4001文字の時、送信ボタンを非活性にする
+    init() {
+        $text
+            .combineLatest($text)
+            .map {
+                let inputText = $0.0
+                guard inputText.isEmpty || inputText.count >= 4001 else { return false }
+                
+                return true
+            }
+            .assign(to: \.toValidation, on: self)
+            .store(in: &anyCancellable)
     }
 }
